6aae0eb003e83aaaeedccb12ec626cc9249d3a5e
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2020-09-19 23:24:49 +0000

    adding features to post section

--- script.js
+++ script.js
@@ -37,7 +37,14 @@ const remove = document.querySelector(".remove");
 add.addEventListener("click", function () {
   const cont = document.querySelector(".menu");
   const input = document.querySelector(".put");
+	  const output = document.querySelector(".area");
+
+	  const area = document.createElement("P");
+		area.setAttribute("class", "lead");
+
   const newli = document.createElement("LI");
+	const  text = document.createElement("H1");
+	text.setAttribute("class" , "text");
 	const parent = document.createElement("DIV");
 	parent.setAttribute("class", "parent-box");
  var file = document.getElementById('file').files[0];
@@ -49,9 +56,16 @@ add.addEventListener("click", function () {
 				image.style.height = "300px";
         // the result image data
         image.src = e.target.result;
+				image.style.marginTop = "-20px";
 				        parent.appendChild(image);
 								parent.appendChild(newli);
-					 newli.innerText = input.value;
+					 newli.append(text);
+					 text.append(input.value);
+					 					 					 newli.append(area);
+
+					 					 area.append(output.value);
+
+					 
      }
      // you have to declare the file loading
 		 				     reader.readAsDataURL(file);
